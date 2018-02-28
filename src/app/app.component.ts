c838fb71f10dcc2b852b766864265774a32d3a80
Prithivi Raj G <prithiviraj1991@gmail.com>
2018-02-28 22:47:32 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  user : User;
+  constructor(){
+    this.user  =new User();
+    this.user.gender="None";
+  }
+
+  onSubmit(){
+    console.log(this.user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
-
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule, MatCheckboxModule,
+   MatOptionModule, MatSelectModule, MatInputModule, 
+   MatMenuModule, MatIconModule, MatToolbarModule, MatCardModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule, MatCheckboxModule, MatOptionModule,
+    MatSelectModule, MatInputModule, BrowserAnimationsModule,
+    MatMenuModule, MatIconModule, MatToolbarModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
